50f9d9efaee0816247860aaa4b509203b0997ab2
Eduardo Pineda <sepineda@gmail.com>
2017-08-31 19:48:19 -0600

    Added components from original wedding repo in .net Core

---
+++ assets/app/app.module.js
@@ -0,0 +1,53 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var router_1 = require("@angular/router");
+var app_component_1 = require("./app.component");
+var home_component_1 = require("./home/home.component");
+var navmenu_component_1 = require("./navmenu/navmenu.component");
+//import 'materialize-css';
+var angular2_materialize_1 = require("angular2-materialize");
+var story_card_component_1 = require("./story-card/story-card.component");
+var confirm_component_1 = require("./confirm/confirm.component");
+var admin_component_1 = require("./admin/admin.component");
+var when_where_component_1 = require("./when-where/when-where.component");
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            declarations: [
+                app_component_1.AppComponent,
+                home_component_1.HomeComponent,
+                navmenu_component_1.NavMenuComponent,
+                story_card_component_1.StoryCardComponent,
+                confirm_component_1.ConfirmComponent,
+                admin_component_1.AdminComponent,
+                when_where_component_1.WhenWhereComponent
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                angular2_materialize_1.MaterializeModule,
+                router_1.RouterModule.forRoot([
+                    { path: '', redirectTo: 'nuestra-historia', pathMatch: 'full' },
+                    { path: 'nuestra-historia', component: home_component_1.HomeComponent },
+                    { path: 'donde-y-cuando', component: when_where_component_1.WhenWhereComponent },
+                    { path: 'confirmar', component: confirm_component_1.ConfirmComponent },
+                    { path: 'boda', component: admin_component_1.AdminComponent },
+                    { path: '**', redirectTo: 'home' }
+                ])
+            ],
+            bootstrap: [app_component_1.AppComponent],
+        })
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,24 +3,36 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent }   from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './navmenu/navmenu.component';
 
 //import 'materialize-css';
 import { MaterializeModule } from "angular2-materialize";
+import { StoryCardComponent } from './story-card/story-card.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { AdminComponent } from './admin/admin.component';
+import { WhenWhereComponent } from './when-where/when-where.component';
 
 @NgModule({
     declarations: [
       AppComponent,
       HomeComponent,
-      NavMenuComponent
+      NavMenuComponent,
+      StoryCardComponent,
+      ConfirmComponent,
+      AdminComponent,
+      WhenWhereComponent
     ],
     imports:      [
       BrowserModule,
       MaterializeModule,
       RouterModule.forRoot([
             { path: '', redirectTo: 'nuestra-historia', pathMatch: 'full' },
-            { path: 'nuestra-historia', component: HomeComponent }
+            { path: 'nuestra-historia', component: HomeComponent },
+            { path: 'donde-y-cuando', component: WhenWhereComponent },
+            { path: 'confirmar', component: ConfirmComponent },
+            { path: 'boda', component: AdminComponent },
+            { path: '**', redirectTo: 'home' }
           ])
     ],
     bootstrap:    [AppComponent],
