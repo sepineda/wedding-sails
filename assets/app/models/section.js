0bf43ec19ca7f53de0ba9811251ad92cece81160
Eduardo Pineda <eduardo.pineda@hpe.com>
2017-09-21 06:16:28 -0600

    Removed js files

--- assets/app/models/section.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SectionCategories;
-(function (SectionCategories) {
-    SectionCategories[SectionCategories["Historia"] = 0] = "Historia";
-    SectionCategories[SectionCategories["Confirmar"] = 1] = "Confirmar";
-    SectionCategories[SectionCategories["Lugar"] = 2] = "Lugar";
-})(SectionCategories = exports.SectionCategories || (exports.SectionCategories = {}));
-//# sourceMappingURL=section.js.map
