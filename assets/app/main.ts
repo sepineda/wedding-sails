b70bea0318a0b31e766d59e2630f3d8bc0292f6b
Eduardo Pineda <sepineda@gmail.com>
2017-10-19 21:52:48 -0600

    Going back to normal compilation

--- assets/app/main.ts
+++ assets/app/main.ts
@@ -1,10 +1,10 @@
 /*Uncomment to use Ahead of time compilation*/
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from './aot/assets/app/app.module.ngfactory';
-
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
-
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { AppModule }              from './app.module';
+// import { platformBrowser } from '@angular/platform-browser';
+// import { AppModuleNgFactory } from './aot/assets/app/app.module.ngfactory';
 //
-// platformBrowserDynamic().bootstrapModule(AppModule);
+// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule }              from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
