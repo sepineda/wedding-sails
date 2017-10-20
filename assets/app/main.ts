5c9f999b71a54433135a99f66dab2b79a70222de
Eduardo Pineda <sepineda@gmail.com>
2017-10-19 21:45:16 -0600

    Going back to AOT compilation

--- assets/app/main.ts
+++ assets/app/main.ts
@@ -1,10 +1,10 @@
 /*Uncomment to use Ahead of time compilation*/
-// import { platformBrowser } from '@angular/platform-browser';
-// import { AppModuleNgFactory } from './aot/assets/app/app.module.ngfactory';
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNgFactory } from './aot/assets/app/app.module.ngfactory';
 
-// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule }              from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { AppModule }              from './app.module';
+//
+// platformBrowserDynamic().bootstrapModule(AppModule);
